cedcdfbb7805ce1aa3ce6ae39090f88af04a9ca4
rav94 <fernandoravindu1994@gmail.com>
2024-02-25 20:59:57 +0530

    Cahnge welcome message

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello WORLD!!!');
+  res.send('Hello SLIIT!!!');
 });
 
 app.listen(config.port, () => {
